4d43d8afd3abe037273d83bc25db4867843ec54b
anna.akimova <anna.akimova@patternmasters.ru>
2024-08-26 22:00:24 +0300

    Start

---
+++ pages/locators.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators():
+    PAGE_LINK = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
+    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
+
